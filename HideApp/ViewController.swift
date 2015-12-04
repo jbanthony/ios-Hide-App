29042edc6ed38213a9b55cf2ab39b9d06a2d1494
Joshua Anthony <halfjoshh@JoshuaAhonysMBP.attlocal.net>
2015-12-04 17:22:19 -0600

    Added HideApp git

--- HideApp/ViewController.swift
+++ HideApp/ViewController.swift
@@ -10,6 +10,12 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var HideSpurs: UIButton!
+    @IBOutlet weak var HideLakers: UIButton!
+    @IBOutlet weak var SpursLogo: UIImageView!
+    @IBOutlet weak var LakersLogo: UIImageView!
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
@@ -20,6 +26,14 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
+    @IBAction func HideSpursAction(sender: AnyObject) {
+        SpursLogo.hidden = true
+    }
+    @IBAction func HideLakersAction(sender: AnyObject) {
+        LakersLogo.hidden = true
+    }
+    
+    
 
 }
 
